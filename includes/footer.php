23479a7d649154dd7cbda5d5432cac2d8b8bbe9d
Søren <soer9428@edu.easj.dk>
2021-10-13 13:04:58 +0200

    - Opsætning af footer - tilføjelse af icon for SoMe

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,26 @@
 <link href="css/menufooter.css" rel="stylesheet" type="text/css">
 
 <footer>
-    <div class="test">Hej hej hej</div>
+    <div class="d-flex justify-content-center">
+    <div class="footer__icon">
+        <a href="#">
+            <img src="img/facebookicon.png" alt="facebook">
+        </a>
+    </div>
+        <div class="footer__icon">
+        <a href="#">
+            <img src="img/linkedinicon.png" alt="LinkedIn">
+        </a>
+        </div>
+    </div>
+    </div>
+
+    <div class="">
+        <p class=""></p>
+        <p></p>
+    </div>
+
+    <p class="copyright text-center my-3">© Define 2021</p>
 </footer>
 
 <script src="node_modules/bo224otstrap/dist/js/bootstrap.bundle.min.js"></script>
