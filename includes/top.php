e088cbfc50a58ab50694348ee0872525f593ed85
David <david.osbeck1996@gmail.com>
2021-10-15 12:32:36 +0200

    Færdiggjort forsiden

--- includes/top.php
+++ includes/top.php
@@ -9,9 +9,9 @@
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
-    <link href="./css/bootstrap.css" rel="stylesheet" type="text/css">
-    <link href="./css/styles.css" rel="stylesheet" type="text/css">
-    <link href="./css/index.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/index.css" rel="stylesheet" type="text/css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/627f0b1d73.js" crossorigin="anonymous"></script>
--- index.php
+++ index.php
@@ -1,32 +1,36 @@
-<?php include 'includes/top.php';?>
 
-
-    <div class="container-fluid header__placeholder__wrapper">
-        <img src="img/headerplaceholder.svg" alt="" class="header__placeholder">
-        <div class="container">
-            <h1 class="display-4 fw-bold lh-1 mb-3"><p>Vi sælger ikke <br><span id="spin"></span></p></h1>
-            <button type="button" class="btn btn-primary btn-lg px-4 me-md-2"><p class="button__text">Fortsæt</p></button>
+<section class="container-fluid hero__section">
+    <?php include 'includes/top.php';?>
+    <div class="container hero__text">
+        <h1 class="display-4 fw-bold lh-1 mb-3"><p>Vi sælger ikke <br><span id="spin"></span></p></h1>
+        <div class="button__container">
+        <button type="button" class="btn btn-primary btn-lg px-4 me-md-2 d-block"><p class="button__text">Fortsæt</p></button>
+            <div class="arrow-container">
+                <a href="#icons__top"><div class="arrow-down"></div></a>
+            </div>
         </div>
     </div>
+</section>
 
-    <div class="icons__top">
+
+    <div class="container-fluid icons__top pb-4" id="icons__top">
         
-        <h1 class="icon__text icon__header__text mb-5 mt-5">Hvad vi kan gøre for dig</h1>
+        <h1 class="icon__text icon__header__text mb-5 mt-5 p-5">Hvad vi kan gøre for dig</h1>
         <div class="row mt-5 me-5 ms-5 mb-3">
-            <div class="col-lg-4 col-md-6 col-sm-12 text-center">
+            <div class="col-lg-4 col-md-6 col-sm-12 text-center d-flex align-items-center flex-column">
                 <i class="fas fa-desktop mb-2"></i>
                 <h1 class="icon__text">Webdesign</h1>
-                <p class="text__color">Alt fra mindre landing pages, til webshops og corporate sites. Vi udvikler en skræddersyet oplevelse til dine brugeres behov.</p>
+                <p class="text__color fs-5 w-75">Mindre landing pages, webshops og corporate sites - Vi udvikler en skræddersyet oplevelse til dine brugeres behov.</p>
             </div>
-            <div class="col-lg-4 col-md-6 col-sm-12 text-center middle__icon">
+            <div class="col-lg-4 col-md-6 col-sm-12 text-center d-flex align-items-center flex-column middle__icon">
                 <i class="fas fa-pencil-ruler mb-2"></i>
                 <h1 class="icon__text">Grafisk design</h1>
-                <p class="text__color">Vi laver design med et formål, som skaber værdi for jeres virksomhed og begejstrer dine brugere.</p>
+                <p class="text__color fs-5 w-75">Vi laver design med et formål; at skabe værdi for jeres virksomhed og begejstrer dine brugere.</p>
             </div>
-            <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0 col-sm-12 mt-md-5 mt-lg-0 text-center">
+            <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0 col-sm-12 mt-md-5 mt-lg-0 text-center d-flex align-items-center flex-column">
                 <i class="fas fa-desktop mb-2"></i>
                 <h1 class="icon__text">SoMe indhold</h1>
-                <p class="text__color">Sociale medier er et afgørende komponent, når marketingstrategien skal udarbejdes, og det kan vi hjælpe med.</p>
+                <p class="text__color fs-5 w-75 ">Sociale medier er et afgørende komponent, når marketingstrategien skal udarbejdes, og det kan vi hjælpe med.</p>
             </div>
         </div>
     </div>
@@ -37,11 +41,11 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-lg-3 offset-lg-3 col-md-5 offset-md-1 col-sm-12 offset-sm-0 text-center">
-                <h1 class="mb-3 text__weight">Digitalt Design</h1>
+            <h1 class="mb-4 mt-4 display-3 text__weight col-lg-6 offset-lg-2">Digitale løsninger til en digital tidsalder.</h1>
+            <div class="col-lg-4 offset-lg-2 col-md-5 offset-md-1 col-sm-12 offset-sm-0">
                 <img src="img/flat%20art.jpg" class="flat__art__small" alt="">
-                <p class="mb-3" style="text-align: left">Vi er et team af kreative fagnørder med forskellige kompetencer og personligheder. Og vi er stolte af det. Vi skaber løsninger der virker, både nu og i fremtiden.</p>
-                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2"><p class="button__text">Læs om os</p></button>
+                <p class="mb-4 fs-5">Vi er et team af kreative fagnørder med forskellige kompetencer og personligheder. Vi er stolte af det! Vi skaber løsninger der virker, både nu og i fremtiden.</p>
+                <a href="omos.php"><button type="button" class="btn btn-primary btn-lg px-4 me-md-2"><p class="button__text">Læs om os</p></button></a>
             </div>
             <div class="col-lg-3 col-md-5 flat__art__big justify-content-start">
                 <img src="img/flat%20art.jpg" alt="">
@@ -54,10 +58,10 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-4 offset-md-4 text-center col-sm-12 offset-sm-0">
-                <h1 class="mb-1 text__weight">Kontakt</h1>
-                <p class="mb-4">Skal vi hjælpe din virksomhed til næste niveau, så kontakt os direkte på telefon eller email, så tager vi en uforpligtende snak omkring dit næste projekt</p>
-                <button type="button" class="btn btn-primary btn-lg px-4 me-md-2"><p class="button__text">Kontakt</p></button>
+            <div class="col-md-5 offset-md-3 text-center col-sm-12 offset-sm-0">
+                <h1 class="mb-4 display-4 text__weight">Kontakt</h1>
+                <p class="mb-4 fs-5">Skal vi hjælpe din virksomhed til næste niveau, så kontakt os direkte på telefon eller email, så tager vi en uforpligtende snak omkring dit næste projekt.</p>
+                <a href="kontakt.php"><button type="button" class="btn btn-primary btn-lg px-4 me-md-2"><p class="button__text">Kontakt</p></button></a>
             </div>
         </div>
     </div>
