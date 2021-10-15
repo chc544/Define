6d837cc778528c4215c98f42425531a451080993
David <david.osbeck1996@gmail.com>
2021-10-15 13:41:38 +0200

    Indsat head tags på forskellige sider

--- omos.php
+++ omos.php
@@ -1,4 +1,37 @@
-<?php include 'includes/top.php';?>
+<!DOCTYPE html>
+<html lang="da" class="h-100">
+<head>
+    <meta charset="utf-8">
+
+    <title>Define Om os</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link href="css/index.css" rel="stylesheet" type="text/css">
+    <link href="css/omos.css" rel="stylesheet" type="text/css">
+    <link href="css/menufooter.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/627f0b1d73.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link href="css/menufooter.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://use.typekit.net/gsh6skh.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body class="d-flex flex-column h-100">
+
+<?php include 'includes/menu.php';?>
 
 <h1 class="text-center my-5">Holdet bag skærmen</h1>
 <section class="d-grid container mb-5">
