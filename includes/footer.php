3917ccafa7f4833968479a1b9a7dd4b418de6715
Søren <soer9428@edu.easj.dk>
2021-10-15 12:28:46 +0200

    - Rettelser til om os siden --> billeder, icons, tekst - tilføjelse af billeder i img mappen - link i menu og footer

--- includes/footer.php
+++ includes/footer.php
@@ -17,10 +17,10 @@
             <div class="footer__menu">
                 <h2 class="footer__menu__header mb-2 mt-4 text-center">Menu</h2>
                     <div class="d-flex flex-column flex-lg-row justify-content-lg-center text-center pb-3">
-                        <a class="fs-5 mx-2 text-decoration-none text-white" href="#">Hjem</a>
+                        <a class="fs-5 mx-2 text-decoration-none text-white" href="index.php">Hjem</a>
                         <a class="fs-5 mx-2 text-decoration-none text-white" href="#">Vi tilbyder</a>
-                        <a class="fs-5 mx-2 text-decoration-none text-white" href="#">Om os</a>
-                        <a class="fs-5 mx-2 text-decoration-none text-white" href="#">Kontakt</a>
+                        <a class="fs-5 mx-2 text-decoration-none text-white" href="omos.php">Om os</a>
+                        <a class="fs-5 mx-2 text-decoration-none text-white" href="kontakt.php">Kontakt</a>
                     </div>
             </div>
         </div>
